fa85cd89acf7301066038266d29c5bce95b4b5dd
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2021-05-10 21:52:15 +0530

    updated test case

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ from ap import app
 
 
 def test_home1():
-    response = app.test_client().get('/edit')
+    response = app.test_client().get('/edit1')
     assert b"To Do App" in response.data
     assert b"Todo Title" in response.data
     assert b"Add" in response.data
